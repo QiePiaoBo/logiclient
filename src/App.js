228a2f5145f067418bb650e40881d7bad0e34afa
duanqilun <duanqilun@mail.jiehun.com>
2023-08-08 18:48:44 +0800

    nextUI init add button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@nextui-org/react';
+
+
+const Component = () => <Button>Click Me</Button>;
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Component />
         <a
           className="App-link"
           href="https://reactjs.org"
